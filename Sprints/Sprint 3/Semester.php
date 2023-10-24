674411396b7b2b7c7f6388caa70ba303524d36b7
zacharycaldwell <113936787+zacharycaldwell@users.noreply.github.com>
2023-10-23 22:22:38 -0400

    Add files via upload

---
+++ Sprints/Sprint 3/Semester.php
@@ -0,0 +1,63 @@
+<!--
+    Project Title: Four Year Plan
+    Course: CSC340
+    School: Bethel University
+    Author: Zachary Caldwell
+    Date: 10/23/23
+    Description: Defines a semester object
+-->
+
+<?php
+    //isComplete is probably not needed.
+    //Need to remove it and related methods later
+    class Semester {
+        //Will re-evaluate the privacy of each field variable later
+        private $isValid;
+        private $isComplete;
+        public $semType;
+        public $courses;
+        public function __construct($season){
+            $this->$semType = $season;
+            $this->$courses = [];
+            $this->$isComplete = false;
+            $this->$isValid = false;
+
+        }
+        //Accessors
+        public function getValidity(){
+            return $this->$isValid;
+        }
+        public function getIsComplete(){
+            return $this->$isComplete;
+        }
+        public function getSemType(){
+            return $this->$semType;
+        }
+        public function getCourse($index){
+            return $this->$courses[$index];
+        }
+        //Mutators
+        public function addCourse($newCourse){
+            array_push($this->courses, $newCourse)
+        }
+        public function setValidity($validity){
+            $this->$isValid = $validity;
+        }
+        public function setComplete($complete){
+            $this->$isComplete = $complete;
+        }
+        //Additional Methods
+        public function toString(){
+            $semString = "";
+            $i=0;
+            //Need to implement a toString() method in course
+            while(i<count(courses)){
+                $semString.=$this->$courses[i].toString();
+                i++;
+            }
+            return $semString;
+
+        }
+        //Need to check validity by verifying each course in this semester is offered in this semester
+    }
+?>
